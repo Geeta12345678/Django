a3f61050f8315ce296f90efeadf7e3d85bf5bfd7
Geeta12345678 <tank.geeta.1026@gmail.com>
2026-01-27 12:14:13 +0530

    Url day2

---
+++ learning26/learning26/views.py
@@ -0,0 +1,14 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+def test(request):
+       return HttpResponse("hello")
+
+def aboutus(request):
+     return render(request,"aboutus.html")
+
+def contactus(request):
+     return render(request,"contactus.html")
+
+def home(request):
+     return render(request,"home.html")
