4b180c6262a563a9c237821fa53b642b5d09883a
Geeta12345678 <tank.geeta.1026@gmail.com>
2026-02-04 14:45:40 +0530

    Models create in differdent table(student,product,category) 04/02/26

--- learning26/student/models.py
+++ learning26/student/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
-# Create your models here.
+# Create your models here
+class student(models.Model):
+    studentName= models.CharField(max_length=100)
+    studentAge= models.IntegerField()
+    studentCity= models.CharField(max_length=40)
+    studentEmail= models.EmailField(null=True)
+       
+    class Meta:
+        db_table="student"
+
+class product(models.Model):
+    productName=models.CharField(max_length=100)
+    productPrice=models.IntegerField()
+    productStoke=models.PositiveIntegerField()
+    productColor=models.CharField(max_length=70,null=True)
+    productStatus=models.BooleanField(default=True)
+    productDescription=models.TextField(null=True)
+
+    class Meta:
+        db_table="product"
+
+class category(models.Model):
+    categoryName=models.CharField(max_length=70)
+    categoryStatus=models.BooleanField(default=True)
+    
+    class Meta:
+        db_table="category"
+    
+
+
+
+
